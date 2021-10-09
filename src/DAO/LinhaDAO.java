5404a52f34d75eb17e0bb0f9a973ae17c600cc3e
Thiago <thiagoandrecardoso@icloud.com>
2021-10-09 13:06:49 -0300

    Small clean code

--- src/DAO/LinhaDAO.java
+++ src/DAO/LinhaDAO.java
@@ -8,7 +8,11 @@ import java.time.LocalTime;
 
 public class LinhaDAO {
 
-    private final String SELECT_BY_ID = "SELECT ID,ORIGEM,DESTINO,HORA_EMBARQUE,HORA_PARTIDA FROM linha WHERE ID=?";
+    private final String SELECT_BY_ID;
+
+    {
+        SELECT_BY_ID = "SELECT ID,ORIGEM,DESTINO,HORA_EMBARQUE,HORA_PARTIDA FROM linha WHERE ID=?";
+    }
 
     public Linha getLineById(int id) {
         Connection connection = null;
@@ -28,12 +32,11 @@ public class LinhaDAO {
             rs = preparedStatement.executeQuery();
             while (rs.next()) {
                 linha = new Linha(rs.getInt("ID"), rs.getString("ORIGEM"),
-                        rs.getString("DESTINO"), rs.getObject(4, LocalTime.class), rs.getObject(5, LocalTime.class));
+                        rs.getString("DESTINO"), rs.getObject(4, LocalTime.class),
+                        rs.getObject(5, LocalTime.class));
             }
-        } catch (SQLDataException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
         } finally {
             ConnDB.CloseResultSet(rs);
             ConnDB.CLosePreparedStatement(preparedStatement);
