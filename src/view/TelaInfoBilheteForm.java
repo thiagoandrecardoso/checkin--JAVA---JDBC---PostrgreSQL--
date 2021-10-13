6dfd0dde2053a723998bb0163beea9c320844716
Thiago <thiagoandrecardoso@icloud.com>
2021-10-13 09:15:35 -0300

    Adicionado Cabeçalho

--- src/view/TelaInfoBilheteForm.java
+++ src/view/TelaInfoBilheteForm.java
@@ -7,12 +7,16 @@ import java.awt.*;
 import java.time.LocalTime;
 
 public class TelaInfoBilheteForm extends JFrame {
+    protected JPanel jPanelCAbecalho;
+    protected JLabel jLabelCabecalho;
+
     protected JPanel jPanelInfoBilhete;
     protected JLabel jLabelOrigem;
     protected JLabel jLabelDestino;
     protected JLabel jLabelHoraPrevistaEmbarque;
     protected JLabel jLabelHoraPrevistaPartida;
     protected JLabel jLabelHoraRelogioParede;
+
     protected JTextField txtNome;
     protected JTextField txtCpf;
     protected JButton btnAtualizarDadosPessoais;
@@ -27,11 +31,21 @@ public class TelaInfoBilheteForm extends JFrame {
 
     private void inicializar() {
         this.setTitle("Informações do bilhete");
-        this.setSize(400, 400);
+        this.setSize(300, 300);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.getContentPane().setLayout(new BorderLayout());
+        this.getContentPane().add(getjPanelCAbecalhoForm(), BorderLayout.PAGE_START);
         this.getContentPane().add(getBilheteForm(), BorderLayout.CENTER);
         this.setResizable(false);
+    }
+
+    public JPanel getjPanelCAbecalhoForm(){
+        if (jPanelCAbecalho == null){
+            jPanelCAbecalho = new JPanel();
+            jLabelCabecalho = new JLabel("Informações da Linha:");
+            jPanelCAbecalho.add(jLabelCabecalho);
+        }
+        return  jPanelCAbecalho;
     }
 
     public JPanel getBilheteForm() {
@@ -42,6 +56,8 @@ public class TelaInfoBilheteForm extends JFrame {
             jLabelHoraPrevistaEmbarque = new JLabel("Hora prevista do embarque: " + bilhete.getLinha().getHora_embarque());
             jLabelHoraPrevistaPartida = new JLabel("Hora prevista da partida: " + bilhete.getLinha().getHora_partida());
             jLabelHoraRelogioParede = new JLabel("Hora atual: " + LocalTime.now());
+
+            jPanelInfoBilhete.setBorder(BorderFactory.createLineBorder(Color.BLUE));
 
             jPanelInfoBilhete.add(jLabelOrigem);
             jPanelInfoBilhete.add(jLabelDestino);
